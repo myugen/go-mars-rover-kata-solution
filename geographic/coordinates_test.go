907b71cf312e27542217211c62f615ab31e72a28
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2022-07-22 00:12:27 +0100

    :white_check_mark: test: add test for scale method

--- geographic/coordinates_test.go
+++ geographic/coordinates_test.go
@@ -31,7 +31,38 @@ func TestCoordinates_Add_Should(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			coordinates := geographic.Coordinates{X: tt.fields.X, Y: tt.fields.Y}
-			assert.Equal(t, tt.want, coordinates.Add(tt.args.coordinates))
+			got := coordinates.Add(tt.args.coordinates)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestCoordinates_Scale(t *testing.T) {
+	type fields struct {
+		X int
+		Y int
+	}
+	type args struct {
+		scale int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   geographic.Coordinates
+	}{
+		{
+			name:   "return coordinates resulting from multiplying the provided scale to current coordinate",
+			fields: fields{X: 2, Y: 4},
+			args:   args{scale: 4},
+			want:   geographic.Coordinates{X: 8, Y: 16},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := geographic.Coordinates{X: tt.fields.X, Y: tt.fields.Y}
+			got := c.Scale(tt.args.scale)
+			assert.Equal(t, tt.want, got)
 		})
 	}
 }
